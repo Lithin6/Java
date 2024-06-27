dfa3d06630777f4851b6c699a1359c9bef511480
Lithin6 <121157571+Lithin6@users.noreply.github.com>
2024-06-27 15:21:38 +0530

    Creating Patterns using java by taking input from the user

--- .github/workflows/Patterns.java
+++ .github/workflows/Patterns.java
@@ -5,7 +5,7 @@ class Patterns{
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+                System.out.print(j+" ");    // add the any symbol like *,+,-,.....
             }
             System.out.println();
         }
